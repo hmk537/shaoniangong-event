20db24bf460d6006f8f4b04427d08b7b7d2e9a2f
houmk <1963440250@qq.com>
2024-01-02 13:46:10 +0800

    华为应用商城审核意见修改

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, LoadingController, App } from 'ionic-angular';
+import { LoginPage } from '../login/login'
 
 import { OrgSelectPage } from '../org-select/org-select'
 /**
@@ -15,7 +16,7 @@ import { OrgSelectPage } from '../org-select/org-select'
 })
 export class SettingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, public loadingCtrl: LoadingController, public appCtrl: App) {
   }
 
   callback = null
@@ -33,6 +34,49 @@ export class SettingPage {
     this.navCtrl.push(OrgSelectPage, {
       from: 'setting'
     })
+  }
+  async cancel() {
+    this.alertCtrl.create({
+      title: '提示',
+      message: '是否确认注销？',
+      buttons: [
+        {
+          text: '否',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: '是',
+          handler: () => {
+            doCancel.call(this)
+          }
+        }
+      ]
+    }).present();
+
+    async function doCancel() {
+      const loading = this.loadingCtrl.create({
+        content: '正在注销...'
+      })
+      loading.present()
+      this.alertCtrl.create({
+        title: '提示',
+        subTitle: '已注销',
+        buttons: [{
+          text: '确认', handler: () => {
+            localStorage.userInfo = null
+            const rootNav = this.appCtrl.getRootNav()
+            rootNav.setPages([{
+              page: LoginPage, params: {
+                from: 'exit'
+              }
+            }])
+          }
+        }]
+      }).present()
+      loading.dismiss()
+    }
   }
 
   get orgName() {
